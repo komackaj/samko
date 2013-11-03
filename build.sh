657d251af5516676098555ca3cda8c8b6aeab267
Juraj Komačka <komackaj@gmail.com>
2013-11-03 13:54:29 +0100

    Add Google Mock + install targets to build/bin

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 BUILD_DIRECTORY="build"
+
 if [ ! -d $BUILD_DIRECTORY ]; then
   mkdir $BUILD_DIRECTORY
 fi
 
-cd build
-cmake .. && make
+cd $BUILD_DIRECTORY
+cmake .. -DCMAKE_INSTALL_PREFIX=.
+make
+make install
