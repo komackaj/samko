8cb4522d4610c0f7e63c23bf94dcd60185847771
Juraj Komačka <komackaj@gmail.com>
2013-11-30 13:24:33 +0100

    Add stringutil split

---
+++ libsamko/include/libsamko/stringutils.h
@@ -0,0 +1,9 @@
+#ifndef SAMKO_STRINGUTILS_H_INCLUDED
+#define SAMKO_STRINGUTILS_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(const std::string& str, std::string separator);
+
+#endif // SAMKO_STRINGUTILS_H_INCLUDED
