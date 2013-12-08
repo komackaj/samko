cbfcce89dd52980edbec72529b5602ff057399fe
Juraj Komačka <komackaj@gmail.com>
2013-12-08 22:55:55 +0100

    Fix: UIException derived from runtime_error + standard constructor

--- libsamko/include/libsamko/ui/uiexception.h
+++ libsamko/include/libsamko/ui/uiexception.h
@@ -1,9 +1,14 @@
 #ifndef UIEXCEPTION_H_INCLUDED
 #define UIEXCEPTION_H_INCLUDED
 
+#include <stdexcept>
 namespace samko{
 
-class UIException : public std::exception{
+class UIException : public std::runtime_error{
+public:
+    explicit UIException(const std::string& __arg)
+        : std::runtime_error(__arg)
+    {};
 };
 
 } //namespace
