28c7f5ba8c22c5b940e38d28f61cd4820f66fe63
Juraj Komačka <juraj.komacka@firma.seznam.cz>
2014-01-04 22:42:32 +0100

    Add failing preciseCircleTest

--- libsamko/unittests/tests/targets/correlrefinertest.cpp
+++ libsamko/unittests/tests/targets/correlrefinertest.cpp
@@ -1,1 +1,8 @@
 #include "targetrefinercommontest.h"
+#include <libsamko/targets/correlrefiner.h>
+
+TEST(CorrelRefinerTest, PreciseCircle) {
+    constexpr size_t RADIUS = 10;
+    CorrelRefiner refiner(2 * RADIUS * 1.2);
+    preciseCircleTest(refiner, RADIUS);
+}
